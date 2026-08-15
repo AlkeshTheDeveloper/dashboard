c7fe0d38e11d48e3357e59bc5e0491aca9915eb4
alkeshthedeveloper <alkeshsakre110791@gmail.com>
2026-08-16 01:51:22 +0530

    Add new prod url and pagesetup

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,27 +1,31 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 api.interceptors.request.use((config) => {
- const persistedState = localStorage.getItem("persist:root");
+  const persistedState = localStorage.getItem("persist:root");
 
-if (persistedState) {
-  const parsedState = JSON.parse(persistedState);
+  if (persistedState) {
+    try {
+      const parsedState = JSON.parse(persistedState);
 
-  if (parsedState.auth) {
-    const auth = JSON.parse(parsedState.auth);
-    const token = auth.token;
+      if (parsedState.auth) {
+        const auth = JSON.parse(parsedState.auth);
+        const token = auth.token;
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+        if (token) {
+          config.headers.Authorization = `Bearer ${token}`;
+        }
+      }
+    } catch (error) {
+      console.error("Failed to read persisted auth:", error);
     }
   }
-}
 
   return config;
 });
--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,18 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Login from "../pages/Login/Login";
 import Dashboard from "../pages/Dashboard/Dashboard";
-import AppLayout from "../components/layout/AppLayout";
+
 import ProtectedRoute from "./ProtectedRoute";
 import PublicRoute from "./PublicRoute";
-import Login from "./../pages/Login/Login";
-import "../index.css";
-import Expenses from "../pages/Expenses/Expenses";
-import Profile from "../pages/Profile/Profile";
-import Budget from "../pages/Budget/Budget";
 
 const AppRoutes = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/dashboard">
       <Routes>
         <Route
           path="/"
@@ -22,20 +18,15 @@ const AppRoutes = () => {
             </PublicRoute>
           }
         />
+
         <Route
+          path="/dashboard"
           element={
             <ProtectedRoute>
-              <AppLayout />
+              <Dashboard />
             </ProtectedRoute>
           }
-        >
-          <Route path="/dashboard" element={<Dashboard />} />
-
-          <Route path="/expenses" element={<Expenses />} />
-
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/budget" element={<Budget />} />
-        </Route>
+        />
       </Routes>
     </BrowserRouter>
   );
