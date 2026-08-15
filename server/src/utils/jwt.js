710f58733a2065ccb31a05c3d0f14e0596e60fbe
alkeshthedeveloper <alkeshsakre110791@gmail.com>
2026-08-16 02:19:37 +0530

    fix url token

--- server/src/utils/jwt.js
+++ server/src/utils/jwt.js
@@ -2,9 +2,11 @@ const jwt = require("jsonwebtoken");
 
 const generateToken = (payload) => {
   return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
+    expiresIn: "7d",
   });
 };
+
+console.log("JWT_EXPIRES_IN:", JSON.stringify(process.env.JWT_EXPIRES_IN));
 
 module.exports = {
   generateToken,
